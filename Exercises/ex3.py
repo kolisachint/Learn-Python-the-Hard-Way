e534c834b6d0d9b22e133e568c4c5590e24245c1
sachin <kolisachint@gmail.com>
2018-10-31 12:44:50 +0000

    Day1

--- Exercises/ex3.py
+++ Exercises/ex3.py
@@ -2,4 +2,33 @@
 # _*_ coding: utf-8 _*_
 # Numbers and Math
 
+print("I will count my chickens:")
+
+print("Hens ", 25 + 30 / 6)
+print("Rosters ",100-25*3%4)
+
+print("Now I will count my eggs:")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("Is it true that 3+2<5-7 ?")
+print(3+2<5-7 )
+
+print("What is 3+2 ?",3+2)
+
+print("What is 5-7 ?",5-7)
+
+print("Oh thats why its false.")
+
+print("How about some more.")
+
+print("Is it greater ?",5> -2)
+print("Is it greater or equal to ?", 5>=-2)
+print("Is is less than or equal to ?", 5 <= -2)
+
+# Python3 does automatic floating point conversion when it sees /
+print("Difference between 7/4 & 7.0/4.0 :",7/4 , 7.0/4.0 )
+print("Difference between 7*4 & 7.0*4.0 :",7*4 , 7.0*4.0 )
+print("Difference between 7**4 & 7.0**4.0 :",7**4,7.0**4.0 )
+print("Difference between 7%4 & 7.0%4.0 :",7%4 , 7.0%4.0 )
 
