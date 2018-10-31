06569b4a50b9ab043067fee4574a45136ece5ae2
kolisachint <kolisachint@gmail.com>
2018-10-31 07:16:12 -0400

    Day1

---
+++ Exercises/ex2.py
@@ -0,0 +1,26 @@
+# A comment, this is so you can read it later
+# Anything after # this is ignored by python
+
+print("I could have code like this.")		# and comment after this is ignored
+
+# You can also use comment to 'disable' or comment out piece of code.
+# print("This won't run.")
+
+print("This will run.")
+
+print("""
+This will still run with #.
+# is considered character when used in code, and when not used as first character of line.
+""")
+
+"""
+This is docstring and will be parsed by interpreter and uses memory as well.
+"""
+'''
+This is docstring and will be parsed by interpreter and uses memory as well.
+'''
+
+# Multiline comments works like this
+# Do not confuse it with docstring use with -> """ or ''' 
+# Docstrings are parsed by interpreter and uses memory. They are no way ignored by python.
+# Multiline block ends here.
