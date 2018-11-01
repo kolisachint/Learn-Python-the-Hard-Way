d2c03621e98dc8cfa6537720bb463b68998d50fb
kolisachint <kolisachint@gmail.com>
2018-11-01 06:54:58 -0400

    Day2

--- Exercises/ex11.py
+++ Exercises/ex11.py
@@ -4,3 +4,32 @@
 
 
 print("How old are you?")
+age=input()
+
+# Indentation is used for block ending for conditional and loop operations
+if age.isdigit():
+	agenum=int(age)
+else:
+	agenum=0
+	print("Please provide valid number for age field:")
+	quit()
+	
+print("How tall are you?")
+height=input()
+print("How much do you weigh?")
+weight=input()
+
+
+print("So you are %d years old, %r in height and %r in weight" % (agenum,height,weight))
+
+
+# Traceback (most recent call last):
+#   File "ex11.py", line 11, in <module>
+#     weight=int(input())
+# ValueError: invalid literal for int() with base 10: 's'
+
+
+#   File "ex11.py", line 17
+#     print("How tall are you?")
+#                              ^
+# IndentationError: unindent does not match any outer indentation level
---
+++ Exercises/ex12.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+# _*_ coding: utf-8 _*_
+# Prompting People
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print("So, you're %r old, %r tall and %r heavy." % (
+age, height, weight))
