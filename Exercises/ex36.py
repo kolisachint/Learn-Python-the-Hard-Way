f41efcdc14804b772734b592856fc34cbb6aea26
kolisachintdummy1 <kolisachintdummy1@gmail.com>
2018-11-05 10:49:49 -0500

    Day4

---
+++ Exercises/ex36.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+# _*_ coding: utf-8 _*_
+# Branches and Functions
+
+i=0
+
+while i==0:
+  print("While Infinite Loop")
+  i += 1                  # infinite loop broke
+
+  
+for i==1:
+  print("For Infinite Loop")
+  i += 1                  # infinite loop broke 
+  
+  
+print("What?")
+  
